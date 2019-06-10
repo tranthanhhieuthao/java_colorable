9a12d7fb146aee0ed92a83b6481108189b7964ec
HieuThao <you@example.com>
2019-06-10 21:24:28 +0700

    bt

--- src/Test.java
+++ src/Test.java
@@ -4,17 +4,12 @@ public class Test {
         shapes[0] =  new Circle(2.1);
         shapes[1] =  new Rectangle(3.5, 2.1);
         shapes[2] =  new square(1.2);
-        Circle circle = (Circle) shapes[0];
-        Rectangle rectangle = (Rectangle) shapes[1];
-        square square = (square) shapes[2];
 
-        for (int i = 0; i < shapes.length; i++) {
-            if (shapes[i] instanceof square) {
-                square.howtocolor();
-            } else {
-                System.out.println(circle.getArea());
-                System.out.println(rectangle.getArea());
+        for (Shape shape :shapes){
+            if (shape instanceof square){
+                ((square) shape).howtocolor();
             }
+            else System.out.println((shape));
         }
     }
 }
